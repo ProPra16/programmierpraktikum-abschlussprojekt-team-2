270d4347c5b84585c898c646b4780c9750e722a9
Marcel <makae103@hhu.de>
2016-06-28 15:38:51 +0200

    created interface for PhaseManager, Phase and PhaseStatus

---
+++ src/main/java/de/hhu/propra16/tddtrainer/logic/Phase.java
@@ -0,0 +1,7 @@
+package de.hhu.propra16.tddtrainer.logic;
+
+public enum Phase {
+
+	RED, GREEN, REFACTOR
+	
+}
---
+++ src/main/java/de/hhu/propra16/tddtrainer/logic/PhaseManagerIF.java
@@ -0,0 +1,26 @@
+package de.hhu.propra16.tddtrainer.logic;
+
+import de.hhu.propra16.tddtrainer.catalog.Exercise;
+
+public interface PhaseManagerIF {
+
+	/**
+	 * Checks if the given code in the exercise is valid or not for the current phase
+	 * @param exercise The Exercise to check
+	 * @param continuePhase if the phase manager should continue to the next phase if the Exercise is valid
+	 * @return a PhaseStatus which contains if the Phase is valid and the ExecutionResult
+	 */
+	public PhaseStatus checkPhase(Exercise exercise, boolean continuePhase);
+	
+	/**
+	 * 
+	 * @return returns the current Phase
+	 */
+	public Phase getPhase();
+	/**
+	 * 
+	 * @return returns the last valid Exercise
+	 */
+	public Exercise resetPhase();
+	
+}
