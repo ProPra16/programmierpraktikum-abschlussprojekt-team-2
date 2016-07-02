a981705ff3a250a5bad24081aba1a4ce7db4b0aa
lulu148 <lusch148@hhu.de>
2016-07-02 20:21:31 +0200

    created tests for PhaseManager and updated PhaseManager

--- src/main/java/de/hhu/propra16/tddtrainer/logic/PhaseManager.java
+++ src/main/java/de/hhu/propra16/tddtrainer/logic/PhaseManager.java
@@ -1,28 +1,49 @@
 package de.hhu.propra16.tddtrainer.logic;
 
 import de.hhu.propra16.tddtrainer.catalog.Exercise;
-import de.hhu.propra16.tddtrainer.executor.ExecutionResult;
-import de.hhu.propra16.tddtrainer.executor.Executor;
+import de.hhu.propra16.tddtrainer.executor.*;
+import de.hhu.propra16.tddtrainer.tracking.TrackingManager;
+import vk.core.api.CompileError;
 
 public class PhaseManager implements PhaseManagerIF {
 	
 	private Phase phase = Phase.RED;
+	private Exercise validExercise;
+	private TrackingManager trackingManager;
+	
+	public PhaseManager(TrackingManager trackingManager) {
+		this.trackingManager = trackingManager;
+	}
 
 	@Override
 	public PhaseStatus checkPhase(Exercise exercise, boolean continuePhase) {
 		ExecutionResult executionResult = new Executor().execute(exercise);
-
+		PhaseStatus phaseStatus;
+		
 		if(phase.equals(Phase.RED)) {
-			if((executionResult.getCompilerResult().hasCompileErrors()) ||
-				(executionResult.getTestResult().getNumberOfFailedTests() == 1)) {
+			boolean valid = true;
+			if(executionResult.getCompilerResult().hasCompileErrors()) {
+				loop:
+				for(CompilationResult cr : executionResult.getCompileErrors()) {
+					for(CompileError ce : cr.getCompileErrors()) {
+						System.out.println(ce.getMessage());
+						if(!(ce.getMessage().contains("cannot find symbol"))) {
+							valid = false;
+							break loop;
+						}
+					}
+				}
+			} 
+			else if(executionResult.getTestResult().getNumberOfFailedTests() != 1) {
+				valid = false;
+			}
+			if(valid) {
 				if(continuePhase) {
 					phase = Phase.GREEN;
 				}
-				return new PhaseStatus(true, executionResult);
+				validExercise = exercise;
 			}
-			else {
-				return new PhaseStatus(false, executionResult);
-			}
+			phaseStatus = new PhaseStatus(valid, executionResult, phase);
 		}
 		
 		else {
@@ -36,12 +57,15 @@ public class PhaseManager implements PhaseManagerIF {
 						phase = Phase.RED;
 					}
 				}
-				return new PhaseStatus(true, executionResult);
+				validExercise = exercise;
+				phaseStatus = new PhaseStatus(true, executionResult, phase);
 			}
 			else {
-				return new PhaseStatus(false, executionResult);
+				phaseStatus = new PhaseStatus(false, executionResult, phase);
 			}
 		}
+		trackingManager.track(exercise, phaseStatus);
+		return phaseStatus;
 	}
 
 	@Override
@@ -51,10 +75,12 @@ public class PhaseManager implements PhaseManagerIF {
 
 	@Override
 	public Exercise resetPhase() {
-		// TODO Auto-generated method stub
-		return null;
+		if(phase.equals(Phase.REFACTOR)) {
+			throw new IllegalStateException("Reset not permitted during Refactor.");
+		}
+		if(phase.equals(Phase.GREEN)) {
+			phase = Phase.RED;
+		}
+		return validExercise;
 	}
-
-	
-	
 }
