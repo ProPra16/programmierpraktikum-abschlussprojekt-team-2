3fc400907ee15d9fb134b2fe3b9dcaafbf294067
Simon Weber <simon.weber@hhu.de>
2016-07-03 14:03:00 +0200

    minor improvements.

--- src/main/java/de/hhu/propra16/tddtrainer/tracking/TrackingManager.java
+++ src/main/java/de/hhu/propra16/tddtrainer/tracking/TrackingManager.java
@@ -1,10 +1,10 @@
 package de.hhu.propra16.tddtrainer.tracking;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 import de.hhu.propra16.tddtrainer.catalog.Exercise;
-import de.hhu.propra16.tddtrainer.logic.Phase;
 import de.hhu.propra16.tddtrainer.logic.PhaseStatus;
 
 public class TrackingManager implements TrackingManagerIF{
@@ -22,7 +22,10 @@ public class TrackingManager implements TrackingManagerIF{
 	public void track(Exercise exercise, PhaseStatus phaseStatus) {
 		Snapshot snapshot = new Snapshot(exercise, LocalDateTime.now(),  phaseStatus);
 		progress.add(snapshot);
-		
+	}
+	
+	static long getTimeBetweenSnaps(LocalDateTime start, LocalDateTime end) {
+		return start.until(end, ChronoUnit.SECONDS);	
 	}
 	
 	public void displayInNewWindow() {
