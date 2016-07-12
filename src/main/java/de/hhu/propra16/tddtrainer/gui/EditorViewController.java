1dea2c42155f9652fceb6590306b4d8d81a71bad
Antonia Pütz <antonia.puetz@hhu.de>
2016-07-12 15:02:27 +0200

    added dialog to show description

--- src/main/java/de/hhu/propra16/tddtrainer/gui/EditorViewController.java
+++ src/main/java/de/hhu/propra16/tddtrainer/gui/EditorViewController.java
@@ -13,6 +13,8 @@ import de.hhu.propra16.tddtrainer.logic.PhaseStatus;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -245,10 +247,6 @@ public class EditorViewController {
 			AnchorPane.setRightAnchor(codeBox, 15.0);
 		}
 		phaseManager.resetPhase();
-	}
-
-	public void showExerciseDescription() {
-
 	}
 
 }
